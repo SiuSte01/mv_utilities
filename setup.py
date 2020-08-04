31a421170e985d7390ea5b4757cf9dbb29781d61
ssiu <Stephen.Siu@risk.LexisNexis.com>
2020-08-04 08:43:33 -0400

    Merging setup.py from: dev into other branches

--- setup.py
+++ setup.py
@@ -9,18 +9,21 @@ setup(name='mvUtil',
       author_email='stephen.siu@lexisnexis.com',
 		package_data={
 			'': ['*.tab','*.txt','*.cfg','*.pm','*.sas','*.xlsm'],
+			'keatonCode/AB': ['*'],
 			'projCode/src': ['dopatients.pl','multiBucket_ABCPM.pl','project_PAC.pl','sendemail.pl','*.sas'],
 				'projCode/src/AdvisoryBoard': ['*'],
 				'projCode/src/INA': ['*'],
 				'projCode/src/Trending': ['*'],
 		},
       packages=[
+			'keatonCode/AB',
 			'projCode/src',
 				'projCode/src/AdvisoryBoard',
 				'projCode/src/INA',
 				'projCode/src/Trending'
 			],
       scripts=[
+			'keatonCode/AB/AB_Tar_Zip',
 			'projCode/src/AdvisoryBoard/combine_inadirs.pl',
 			'projCode/src/AdvisoryBoard/combinesummaries_input.pl',
 			'projCode/src/AdvisoryBoard/comparesizes_input.pl',
